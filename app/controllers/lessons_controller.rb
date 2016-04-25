c08d83469b21cc66cab0be6947cd172a994d9421
Ziyue <catherine000000@gmail.com>
2016-04-25 03:42:15 +0000

    require users to be enrolled in a couse to view the lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,21 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_course_enrollment_for_lesson, only: :show
   def show
   end
 
   private
+
+  def require_course_enrollment_for_lesson
+    current_course = current_lesson.section.course
+
+    if current_user && current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'Please enroll in this course to view its lessons'
+    end
+  end
+
+
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
-    end  
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
